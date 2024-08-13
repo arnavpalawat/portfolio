0d9383814e969c4b1aba7f4091b93ec6384197bd
Arnav Palawat <81100074+arnavpalawat@users.noreply.github.com>
2024-08-13 14:46:35 -0400

    Update ExpCards.tsx

--- components/ExpCards.tsx
+++ components/ExpCards.tsx
@@ -109,7 +109,7 @@ export default function ExpCard() {
       <div className="lg:px-12 mt-12">
         <a
           className="inline-flex items-center font-medium leading-tight text-foreground group"
-          href="/am-resume-2024.pdf"
+          href="/arnavpalawat_cv.pdf"
         >
           <span className="border-b border-transparent pb-px transition hover:border-primary motion-reduce:transition-none">
             View Full Resume

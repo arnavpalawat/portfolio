a006984be0bf3dca78bace10374718c07cb6f16a
Arnav Palawat <81100074+arnavpalawat@users.noreply.github.com>
2024-08-13 14:20:22 -0400

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -10,34 +10,32 @@ export default function About() {
       </div>
       <div className="flex flex-col gap-4">
         <p className="text-start text-muted-foreground lg:px-6">
-          I embarked on my tech journey at 17, diving into web development with
-          a passion for bringing ideas to life. This passion led me to design
-          and develop various websites as a freelancer. After military service,
-          I ventured into quality assurance within the engineering sector,
-          focusing on renewable energy projects.
+          I embarked on my coding journey at 12, diving into app development with flutter, and
+          a burning passion for bringing concepts from ideation to creation. This passion led me to design
+          and develop various apps, be it for massive political parties, or the U.S. Congress. From 15-16 I delved into,
+          Python scripts, learning Ai and ML, and web scraping,
+          focusing on community centered projects.
         </p>
         <p className="text-start  text-muted-foreground lg:px-6">
-          In parallel, my path took an exciting turn into the Web3 space,
-          co-founding{" "}
+          In parallel, my path took an exciting turn into the non-profit space,
+          founding{" "}
           <a
             className="no-wrap text-primary dark:text-white"
-            href="https://swoop.exchange"
+            href="https://internhs.com"
             target="blank"
             rel="noopener noreferrer"
           >
-            Swoop Exchange
+            internhs
           </a>{" "}
-          two years ago, in a mission to tackle the fragmentation of liquidity
-          and protocols and simplify the Decentralized Finance onboarding
-          process for the next billion users. Our platform has quickly reached
-          critical milestones, boasting a trading volume of millions USD and
-          attracting over 4,000 monthly active users.
+          this year, providing high-school students with opportunities
+          to develop in the workplace in a professional setting
         </p>
         <p className="text-start text-muted-foreground lg:px-6">
-          Outside of work, I&#39;m an avid reader of blockchain and cryptography
-          and enjoy my time outdoors. Looking forward, I aim to leverage my
-          frontend development and quality assurance expertise to contribute to
-          impactful tech ventures.
+          Outside of coding, I&#39;m an avid entrepreneur who has started 
+          multiple business' with interest in finance, furthermore, I enjoy my time 
+          going outside, working out, and playing volleyball. Looking forward, 
+          I aim to leverage my development experience and entrepreneuership and 
+          financial experience to build startups and investment algorithms.
         </p>
       </div>
     </section>

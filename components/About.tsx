269d950bfe9794a84e10af1a4709829c77f17bc7
Arnav Palawat <81100074+arnavpalawat@users.noreply.github.com>
2024-08-13 15:09:08 -0400

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@
 
 export default function About() {
   return (
-    <section id="about" className="scroll-mt-16 ">
+    <section id="about" className="scroll-mt-16">
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
           About
@@ -10,13 +10,13 @@ export default function About() {
       </div>
       <div className="flex flex-col gap-4">
         <p className="text-start text-muted-foreground lg:px-6">
-          I embarked on my coding journey at 12, diving into app development with flutter, and
+          I embarked on my coding journey at 12, diving into app development with Flutter, and
           a burning passion for bringing concepts from ideation to creation. This passion led me to design
-          and develop various apps, be it for massive political parties, or the U.S. Congress. From 15-16 I delved into,
-          Python scripts, learning Ai and ML, and web scraping,
-          focusing on community centered projects.
+          and develop various apps, be it for massive political parties, or the U.S. Congress. From 15-16 I delved into
+          Python scripts, learning AI and ML, and web scraping,
+          focusing on community-centered projects.
         </p>
-        <p className="text-start  text-muted-foreground lg:px-6">
+        <p className="text-start text-muted-foreground lg:px-6">
           In parallel, my path took an exciting turn into the non-profit space,
           founding{" "}
           <a
@@ -27,14 +27,14 @@ export default function About() {
           >
             internhs
           </a>{" "}
-          this year, providing high-school students with opportunities
-          to develop in the workplace in a professional setting
+          this year, providing high school students with opportunities
+          to develop in the workplace in a professional setting.
         </p>
         <p className="text-start text-muted-foreground lg:px-6">
-          Outside of coding, I&#39;m an avid entrepreneur who has started 
-          multiple business' with interest in finance, furthermore, I enjoy my time 
+          Outside of coding, I&apos;m an avid entrepreneur who has started 
+          multiple businesses with interest in finance. Furthermore, I enjoy my time 
           going outside, working out, and playing volleyball. Looking forward, 
-          I aim to leverage my development experience and entrepreneuership and 
+          I aim to leverage my development experience, entrepreneurship, and 
           financial experience to build startups and investment algorithms.
         </p>
       </div>

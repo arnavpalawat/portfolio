1919a7a630837389fb0471ae0260ea3013159331
Arnav Palawat <81100074+arnavpalawat@users.noreply.github.com>
2024-08-13 21:33:20 -0400

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -46,7 +46,7 @@ export default function Nav() {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 flex flex-col lg:gap-4">
       <div className="flex flex-col gap-4 lg:pr-24 mt-6 lg:mt-0">
         <h1 className="text-[42px] font-bold lg:text-start">
-          Hey, I&#39;m Arnav Palawat 👋
+          Hey, I&#39;m Arnav 👋
         </h1>
         <h2 className="text-xl lg:text-start">
           Entrepreneur, Software Developer, Founder, Aspiring Consultant and Prospective VC Investor.
